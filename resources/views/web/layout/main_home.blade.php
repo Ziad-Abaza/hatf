5caca4e50e1034fff10e68fc86806c99ad46c0e5
zeyad.h.abaza <zeyad.h.abaza@gmail.com>
2025-04-24 21:52:08 +0200

    fix: Update favicon links to include versioning for cache management

--- resources/views/web/layout/main_home.blade.php
+++ resources/views/web/layout/main_home.blade.php
@@ -24,7 +24,7 @@
     <!--/>-->
     {{-- <link rel="icon" href="{{ asset('front/assets/imgs/favicon.svg') }}" type="image/svg+xml" /> --}}
     <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('front/assets/imgs/forces.png') }}">
-    <link rel="icon" href="{{ asset('front/assets/imgs/favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('front/assets/imgs/favicon.ico?v=2') }}" type="image/x-icon">
 
     <link rel="alternate" href="https://hatf.sa/" hreflang="ar-sa"  />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.css" rel="stylesheet" />
